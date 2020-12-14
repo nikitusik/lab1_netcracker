2c0efe4c58aeb59cc6cfcb0301698f415c880ab8
Никита Ярый <nikitacpmi@gmail.com>
2020-12-14 18:22:14 +0400

    fix: add support work with json files

--- src/main/java/project/netcracker/dao/StudentDaoImpl.java
+++ src/main/java/project/netcracker/dao/StudentDaoImpl.java
@@ -1,44 +1,54 @@
 package project.netcracker.dao;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import project.netcracker.model.Student;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class StudentDaoImpl implements StudentDao {
+
+    private final static String studentFile = "student.json";
+
     private final ObservableList<Student> students = FXCollections.observableArrayList();
-    //private static final List<Student> students = new ArrayList<>();
 
-    public void create(Student student) {
-        boolean isExist = false;
-        for (Student s : students) {
-            if (s.getId() == student.getId()) {
-                isExist = true;
-                break;
-            }
-        }
-        if (!isExist) {
-            students.add(student);
-        }
+    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private final ObjectReader objectReader = objectMapper.reader().forType(new TypeReference<List<Student>>() {});
+    private final ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter());
 
+    public void create(Student student) throws IOException {
+        AtomicInteger AUTO_ID = new AtomicInteger(students.size());
+        student.setId(AUTO_ID.getAndIncrement());
+        students.add(student);
+        saveStudents();
     }
 
 
-    public void delete(int id) {
+    public void delete(int id) throws IOException {
         students.removeIf(student -> student.getId() == id);
+        saveStudents();
     }
 
-    public void edit(Student student) {
+    public void edit(Student student) throws IOException {
 
         students.set(student.getId(), student);
+        saveStudents();
     }
-    public Student getStudentByName(String name){
-        for(Student student: students){
-            if(student.getName().equals(name)){
+
+    public Student getStudentByName(String name) {
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
                 return student;
             }
         }
@@ -49,7 +59,17 @@ public class StudentDaoImpl implements StudentDao {
         return students.get(id);
     }
 
-    public List<Student> getAll() {
+    public List<Student> getAll() throws IOException {
+        loadStudents();
         return students;
+    }
+
+    public void loadStudents() throws IOException {
+        List<Student> tempList = objectReader.readValue(new File(studentFile));
+        students.setAll(tempList);
+    }
+
+    public void saveStudents() throws IOException {
+        objectWriter.writeValue(new File(studentFile), students);
     }
 }
