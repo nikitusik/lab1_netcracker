9657f757273aedd00b30bc6940446a38073507e9
Никита Ярый <nikitacpmi@gmail.com>
2020-12-14 22:40:15 +0400

    add: add initialize() for show data on listView about students and groups. Edit method findNumberGroupAction()

--- src/main/java/project/netcracker/controller/MainController.java
+++ src/main/java/project/netcracker/controller/MainController.java
@@ -1,5 +1,6 @@
 package project.netcracker.controller;
 
+import javafx.collections.FXCollections;
 import project.netcracker.Main;
 import project.netcracker.dao.GroupDao;
 import project.netcracker.dao.GroupDaoImpl;
@@ -20,7 +21,6 @@ import static project.netcracker.controller.AlertConstant.*;
 public class MainController {
     public Button createGroup;
     public Button deleteGroup;
-    public Button findAllGroup;
     public Button editGroup;
     public Button findNumberGroup;
 
@@ -28,7 +28,6 @@ public class MainController {
 
     public Button createStudent;
     public Button deleteStudent;
-    public Button findAllStudent;
     public Button editStudent;
     public Button findNameStudent;
 
@@ -36,19 +35,24 @@ public class MainController {
     public TextField textNameStudent;
     public TextField textNumberGroup;
 
-    StudentDao studentDao = new StudentDaoImpl();
-    GroupDao groupDao = new GroupDaoImpl();
+    private final StudentDao studentDao = new StudentDaoImpl();
+    private final GroupDao groupDao = new GroupDaoImpl();
     private Main main;
 
     public MainController() throws IOException {
     }
 
-    public void setMain(Main main) {
+    private void initialize() throws IOException {
+        listGroup.setItems((ObservableList<Group>) groupDao.getAll());
+        listStudents.setItems((ObservableList<Student>) studentDao.getAll());
+    }
+
+    public void setMain(Main main) throws IOException {
         this.main = main;
+        initialize();
     }
 
     public void createGroupAction(ActionEvent actionEvent) throws IOException {
-
         Group group = new Group();
         boolean okClicked = main.showGroupEditDialog(group);
         if (okClicked) {
@@ -57,19 +61,10 @@ public class MainController {
     }
 
     public void deleteGroupAction(ActionEvent actionEvent) throws IOException {
-
         Group selectedGroup = listGroup.getSelectionModel().getSelectedItem();
         if (selectedGroup != null) {
             groupDao.delete(selectedGroup);
         }
-    }
-
-    public void findAllGroupAction(ActionEvent actionEvent) throws IOException {
-        listGroup.setItems((ObservableList<Group>) groupDao.getAll());
-        createGroup.setDisable(false);
-        editGroup.setDisable(false);
-        deleteGroup.setDisable(false);
-        findNumberGroup.setDisable(false);
     }
 
     public void editGroupAction(ActionEvent actionEvent) throws IOException {
@@ -87,38 +82,39 @@ public class MainController {
         Group group = groupDao.getByNumber(textNumberGroup.getText());
         listGroup.scrollTo(group);
         listGroup.getSelectionModel().select(group);
+
+        ObservableList<Student> students = (ObservableList<Student>) studentDao.getAll();
+        ObservableList<Student> result = FXCollections.observableArrayList();
+
+        for (Student student : students) {
+            if (student.getGroup().getNumber().equals(group.getNumber())) {
+                result.add(student);
+            }
+        }
+        main.showListStudentForGroup(result);
+        textNumberGroup.setText(null);
     }
 
     public void createStudentAction(ActionEvent actionEvent) throws IOException {
-
         Student student = new Student();
         boolean okClicked = main.showStudentEditDialog(student, groupDao);
-        if (okClicked){
+        if (okClicked) {
             studentDao.create(student);
         }
-
     }
 
     public void deleteStudentAction(ActionEvent actionEvent) throws IOException {
         Student selectedStudent = listStudents.getSelectionModel().getSelectedItem();
-        if(selectedStudent != null){
+        if (selectedStudent != null) {
             studentDao.delete(selectedStudent.getId());
         }
     }
 
-    public void findAllStudentAction(ActionEvent actionEvent) throws IOException {
-        listStudents.setItems((ObservableList<Student>) studentDao.getAll());
-        createStudent.setDisable(false);
-        editStudent.setDisable(false);
-        deleteStudent.setDisable(false);
-        findNameStudent.setDisable(false);
-    }
-
     public void editStudentAction(ActionEvent actionEvent) throws IOException {
         Student selectedStudent = listStudents.getSelectionModel().getSelectedItem();
-        if(selectedStudent != null){
+        if (selectedStudent != null) {
             boolean okClicked = main.showStudentEditDialog(selectedStudent, groupDao);
-            if (okClicked){
+            if (okClicked) {
                 studentDao.edit(selectedStudent);
             }
         }
@@ -128,14 +124,15 @@ public class MainController {
         Student student = studentDao.getStudentByName(textNameStudent.getText());
         listStudents.scrollTo(student);
         listStudents.getSelectionModel().select(student);
+        textNameStudent.setText(null);
     }
 
-    private void showAlertInformation(String title, String result, String content) {
-
+    private void showAlertInformation() throws IOException {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setHeaderText("Results: " + result);
-        alert.setContentText(content);
+        alert.setTitle("title");
+        alert.setHeaderText("Results: ");
+
+        alert.setContentText("Hello");
 
         alert.showAndWait();
     }
